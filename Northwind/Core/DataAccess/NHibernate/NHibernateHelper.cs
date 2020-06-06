5a6993cc66e3a1c96c14ff4ddc043379f37c196f
Yasin Aksu <yasinaksu88@gmail.com>
2020-06-06 18:06:53 +0300

    NHibernateHelper base class was written for Session Managment

---
+++ Northwind/Core/DataAccess/NHibernate/NHibernateHelper.cs
@@ -0,0 +1,29 @@
+﻿using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.NHibernate
+{
+    public abstract class NHibernateHelper : IDisposable
+    {
+        private static ISessionFactory _sessionFactory;
+
+        //protected yapılabilir
+        public virtual ISessionFactory SessionFactory => _sessionFactory ?? (_sessionFactory = InitializeFactory());
+
+        protected abstract ISessionFactory InitializeFactory();
+
+        public virtual ISession OpenSession()
+        {
+            return SessionFactory.OpenSession();
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
