61920ce83924bc403cecde1f34ad9e15823d0538
Yasin Aksu <yasinaksu88@gmail.com>
2020-06-06 17:52:04 +0300

    IQueryableRepository was implemented for EntityFramework

---
+++ Northwind/Core/DataAccess/EntityFramework/EfQueryableRepository.cs
@@ -0,0 +1,25 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EfQueryableRepository<T> : IQueryableRepository<T> where T : class, IEntity, new()
+    {
+        private readonly DbContext _context;
+        private IDbSet<T> _entities;
+        public EfQueryableRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<T> Table => this.Entities;
+
+        protected virtual IDbSet<T> Entities => _entities ?? (_entities = _context.Set<T>());
+    }
+}
