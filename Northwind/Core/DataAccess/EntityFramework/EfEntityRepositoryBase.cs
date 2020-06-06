385830bad6cdc7710fe73e48eb91746a3a3e08b0
Yasin Aksu <yasinaksu88@gmail.com>
2020-06-06 17:40:40 +0300

    IEntityRepository was  implemented for EntityFramework

--- Northwind/Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
+++ Northwind/Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Migrations.Model;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
@@ -9,33 +10,58 @@ using System.Threading.Tasks;
 
 namespace Core.DataAccess.EntityFramework
 {
-    public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+    public abstract class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
         where TEntity : class, IEntity, new()
         where TContext : DbContext, new()
     {
         public TEntity Add(TEntity entity)
         {
-            throw new NotImplementedException();
+            using (var context=new TContext())
+            {
+                var added = context.Entry(entity);
+                added.State = EntityState.Added;
+                context.SaveChanges();
+                return entity;
+            }
         }
 
         public void Delete(TEntity entity)
         {
-            throw new NotImplementedException();
+            using (var context=new TContext())
+            {
+                var deleted = context.Entry(entity);
+                deleted.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
         }
 
         public TEntity Get(Expression<Func<TEntity, bool>> filter)
         {
-            throw new NotImplementedException();
+            using (var context=new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
         }
 
         public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
         {
-            throw new NotImplementedException();
+            using (TContext context=new TContext())
+            {
+                return filter == null
+                    ? context.Set<TEntity>().ToList()
+                    : context.Set<TEntity>().Where(filter).ToList();
+            }
         }
 
         public TEntity Update(TEntity entity)
         {
-            throw new NotImplementedException();
+            using (var context=new TContext())
+            {
+                var updated = context.Entry(entity);
+                updated.State = EntityState.Modified;
+                context.SaveChanges();
+                return entity;
+            }
         }
     }
 }
