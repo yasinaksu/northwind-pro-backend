94d9e17571aba8fbe794cf694aeb998071dcb5d0
Yasin Aksu <yasinaksu88@gmail.com>
2020-06-07 15:27:35 +0300

    NhProductDal was tested in DataAccess.Tests and test succed.

---
+++ Northwind/Northwind.DataAccess.Tests/NHibernateTests/NhProductDalTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Northwind.DataAccess.Concrete.NHibernate;
+using Northwind.DataAccess.Concrete.NHibernate.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.DataAccess.Tests.NHibernateTests
+{
+    [TestClass]
+    public class NhProductDalTests
+    {
+        [TestMethod]
+        public void Get_all_returns_all_products()
+        {
+            NhProductDal productDal = new NhProductDal(new SqlServerHelper());
+            var result = productDal.GetAll();
+
+            Assert.AreEqual(77, result.Count);
+        }
+
+        [TestMethod]
+        public void Get_all_with_predicate_returns_filtered_products()
+        {
+            NhProductDal productDal = new NhProductDal(new SqlServerHelper());
+            var result = productDal.GetAll(p => p.ProductName.Contains("ab"));
+
+            Assert.AreEqual(4, result.Count);
+        }
+    }
+}
