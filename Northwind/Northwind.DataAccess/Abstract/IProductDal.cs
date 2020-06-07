c5c27c3a6e7869c4fda658d3180dff50d6690c8c
Yasin Aksu <yasinaksu88@gmail.com>
2020-06-07 11:38:41 +0300

    IProductDal  and Product domain created commit

---
+++ Northwind/Northwind.DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,14 @@
+﻿using Core.DataAccess;
+using Northwind.Entities.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.DataAccess.Abstract
+{
+    public interface IProductDal : IEntityRepository<Product>
+    {
+    }
+}
---
+++ Northwind/Northwind.Entities/Domains/Product.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Entities.Domains
+{
+    public class Product : IEntity
+    {
+        public virtual int ProductId { get; set; }
+        public virtual int  CategoryId { get; set; }
+        public virtual string ProductName { get; set; }
+        public virtual string QuantityPerUnit { get; set; }
+        public virtual decimal UnitPrice { get; set; }
+
+    }
+}
