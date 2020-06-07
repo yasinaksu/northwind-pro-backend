7e18d13e5cba09a740753b8574eaa6eed615462b
Yasin Aksu <yasinaksu88@gmail.com>
2020-06-07 15:21:56 +0300

    IProductDal was implemented to NhProductDal for Nhibernate

---
+++ Northwind/Northwind.DataAccess/Concrete/NHibernate/NhProductDal.cs
@@ -0,0 +1,19 @@
+﻿using Core.DataAccess.NHibernate;
+using Northwind.Entities.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.DataAccess.Concrete.NHibernate
+{
+    public class NhProductDal : NhEntityRepositoryBase<Product>
+    {
+        private readonly NHibernateHelper _nHibernateHelper;
+        public NhProductDal(NHibernateHelper nHibernateHelper) : base(nHibernateHelper)
+        {
+            _nHibernateHelper = nHibernateHelper;
+        }
+    }
+}
