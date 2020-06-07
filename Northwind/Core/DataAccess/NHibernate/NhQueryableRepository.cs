10603e82f026ae9970802e04a193437e783305c4
Yasin Aksu <yasinaksu88@gmail.com>
2020-06-07 11:21:10 +0300

    IQueryableRepository was implemented for NhQuerableRepository commit.

---
+++ Northwind/Core/DataAccess/NHibernate/NhQueryableRepository.cs
@@ -0,0 +1,25 @@
+﻿using Core.Entities;
+using NHibernate.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.NHibernate
+{
+    public class NhQueryableRepository<T> : IQueryableRepository<T> where T : class, IEntity, new()
+    {
+        private readonly NHibernateHelper _nHibernateHelper;
+        private IQueryable<T> _entities;
+        public NhQueryableRepository(NHibernateHelper nHibernateHelper)
+        {
+            _nHibernateHelper = nHibernateHelper;
+        }
+
+        //Engin hoca burda virtual yapmamış ?
+        public virtual IQueryable<T> Table => this.Entities;
+
+        public virtual IQueryable<T> Entities => _entities ?? (_entities = _nHibernateHelper.OpenSession().Query<T>());
+    }
+}
