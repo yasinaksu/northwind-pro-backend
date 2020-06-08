b91126b3996ccf801afbfa8883aade3a82015e99
Yasin Aksu <yasinaksu88@gmail.com>
2020-06-08 14:54:17 +0300

    LogParameter was created.

---
+++ Northwind/Core/CrossCuttingConcerns/Logging/LogParameter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Logging
+{
+    /// <summary>
+    /// This class keep Method to log parameter detail.
+    /// </summary>
+    public class LogParameter
+    {
+        /// <summary>
+        /// Method parameter name
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Method parameter type.
+        /// </summary>
+        public string Type { get; set; }
+        /// <summary>
+        /// Method parameter value.
+        /// </summary>
+        public object Value { get; set; }
+
+
+    }
+}
