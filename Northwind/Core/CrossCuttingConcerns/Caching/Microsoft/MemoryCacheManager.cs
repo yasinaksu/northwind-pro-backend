ae5bb895d67c08ce5b9a2196ed1305528c5b6c31
Yasin Aksu <yasinaksu88@gmail.com>
2020-06-08 13:15:52 +0300

    ICacheManager was implemented by MemoryCachManager for Microsoft Cache Mechanism.

---
+++ Northwind/Core/CrossCuttingConcerns/Caching/Microsoft/MemoryCacheManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Caching.Microsoft
+{
+    public class MemoryCacheManager : ICacheManager
+    {
+        protected ObjectCache Cache => MemoryCache.Default;
+        public void Add(string key, object data, int cacheTime = 60)
+        {
+            if (data == null)
+            {
+                return;
+            }
+            var policy = new CacheItemPolicy
+            {
+                AbsoluteExpiration = DateTime.Now.AddMinutes(cacheTime),
+
+            };
+            var cacheItem = new CacheItem(key, data);
+            Cache.Add(cacheItem, policy);
+        }
+
+        public void Clear()
+        {
+            foreach (var item in Cache)
+            {
+                Remove(item.Key);
+            }
+        }
+
+        public T Get<T>(string key)
+        {
+            return (T)Cache[key];
+        }
+
+        public bool IsAdd(string key)
+        {
+            return Cache.Contains(key);
+        }
+
+        public void Remove(string key)
+        {
+            Cache.Remove(key);
+        }
+
+        public void RemoveByPattern(string pattern)
+        {
+            var regex = new Regex(pattern, RegexOptions.Singleline | RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            var matchedKeyList = Cache.Where(c => regex.IsMatch(c.Key)).Select(c=>c.Key).ToList();
+            foreach (var key in matchedKeyList)
+            {
+                Remove(key);
+            }
+        }
+    }
+}
