51fda09573d1b08b6ce60772f00fa70f013899ec
Yasin Aksu <yasinaksu88@gmail.com>
2020-06-10 14:46:23 +0300

    BusinessProfile was inherited from AutoMapper.Profile for IOC

---
+++ Northwind/Northwind.Business/Mappers/AutoMapper/Profiles/BusinessProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Northwind.Entities.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Business.Mappers.AutoMapper.Profiles
+{
+    public class BusinessProfile : Profile
+    {
+        public BusinessProfile()
+        {
+            CreateMap<Product,Product>()
+        }
+    }
+}
