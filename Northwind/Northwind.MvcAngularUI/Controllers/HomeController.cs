5fff3409473206628a4600c7e2085eb15a25a83a
Yasin Aksu <yasinaksu88@gmail.com>
2020-06-11 13:55:20 +0300

    delete angular project

--- Northwind/Northwind.MvcAngularUI/Controllers/HomeController.cs
+++ Northwind/Northwind.MvcAngularUI/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Northwind.MvcAngularUI.Controllers
+namespace DevFramework.Northwind.MvcAngularUI.Controllers
 {
     public class HomeController : Controller
     {
