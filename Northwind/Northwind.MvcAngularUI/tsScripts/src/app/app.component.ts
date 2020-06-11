797861020f1904998c8c0045000a674870e12c7e
Yasin Aksu <yasinaksu88@gmail.com>
2020-06-11 14:00:39 +0300

    angular remove completely

--- Northwind/Northwind.MvcAngularUI/tsScripts/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: 'tsScripts/src/app/app.component.html'
-})
-export class AppComponent {
-  title = 'My First Angular App';
-}
